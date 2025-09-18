8afe0ac73eacbaed47aa17a4842a27affe2e60b5
winsep <winsep2004@gmail.com>
2025-09-19 02:35:14 +0700

    update

--- themes/pepsico/header.php
+++ themes/pepsico/header.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Font (tuỳ chọn) -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700&display=swap" rel="stylesheet">
   <?php if ( ! current_theme_supports('title-tag') ) : ?>
     <title><?php echo esc_html( wp_get_document_title() ); ?></title>
   <?php endif; ?>
